bc9d1d3841932b574d4264f4520ba174947a2379
gyurikol <gyuri.kol@outlook.com>
2017-05-14 21:26:59 -0700

    Boolean-> bool

--- SIT321 Assignment 3 WPF/Users/Student.cs
+++ SIT321 Assignment 3 WPF/Users/Student.cs
@@ -4,7 +4,7 @@ namespace SIT321_Assignment_3_WPF.Users
 {
     class Student : User
     {
-        public Boolean AtRisk { get; set; }
+        public bool AtRisk { get; set; }
         public decimal Performance { get; set; }
         public int Attendance { get; set; }
     }
