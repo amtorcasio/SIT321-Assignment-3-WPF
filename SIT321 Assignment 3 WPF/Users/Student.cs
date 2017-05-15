666dfd4d2cccea964d1c44d1aef15583daf75ca6
gyurikol <gyuri.kol@outlook.com>
2017-05-14 21:38:19 -0700

    constructors. protected password

--- SIT321 Assignment 3 WPF/Users/Student.cs
+++ SIT321 Assignment 3 WPF/Users/Student.cs
@@ -4,6 +4,15 @@ namespace SIT321_Assignment_3_WPF.Users
 {
     class Student : User
     {
+        // constructor
+        public Student(string fname, string lname, string email, string password)
+        {
+            Firstname = fname;
+            Lastname = lname;
+            Email = email;
+            Password = password;
+        }
+
         public bool AtRisk { get; set; }
         public decimal Performance { get; set; }
         public int Attendance { get; set; }
