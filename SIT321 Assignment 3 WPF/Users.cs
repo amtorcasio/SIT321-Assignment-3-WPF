87f20931898fc64326d0132652e44378f50fdf64
gyurikol <gyuri.kol@outlook.com>
2017-05-14 20:16:06 -0700

    people model struct

---
+++ SIT321 Assignment 3 WPF/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT321_Assignment_3_WPF
+{
+    class User
+    {
+
+    }
+
+    class Administrator : User
+    {
+
+    }
+
+    class Lecturer : User
+    {
+
+    }
+
+    class Student : User
+    {
+
+    }
+}
