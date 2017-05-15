7d5beefa2c6a3477d82853e0ecc069102fc52769
Andrea Michele Torcasio <torcasioandreamichele@hotmail.it>
2017-05-15 14:19:09 +1000

    Added System.Data.Linq GetDatabaseSqlConnection and GetDatabaseDataContext added

--- SIT321 Assignment 3 WPF/Users/User.cs
+++ SIT321 Assignment 3 WPF/Users/User.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.Linq;
 
 namespace SIT321_Assignment_3_WPF.Users
 {
@@ -10,5 +12,16 @@ namespace SIT321_Assignment_3_WPF.Users
         public string Email;
         private string Password;
         public List<Unit> Units;
+
+        public string PATH_DATA = System.AppDomain.CurrentDomain.BaseDirectory + "\\Data\\";
+        private SqlConnection GetDatabaseSQLConnection()
+        {
+            return new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='" + PATH_DATA + "Database.mdf';Integrated Security=True");
+        }
+
+        private DataContext GetDatabaseDataContext()
+        {
+            return new DataContext(@"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename='" + PATH_DATA + "Database.mdf'; Integrated Security=True");
+        }
     }
 }
