f3af58df1970e27999176dd18b617cb1e5ec37a4
Andrea Michele Torcasio <torcasioandreamichele@hotmail.it>
2017-05-16 13:05:01 +1000

    Added SQLite - Converted LocalDB to SQLlite

--- SIT321 Assignment 3 WPF/Users/User.cs
+++ SIT321 Assignment 3 WPF/Users/User.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data.Linq;
-using System.IO;
 
 namespace SIT321_Assignment_3_WPF.Users
 {
@@ -27,15 +26,16 @@ namespace SIT321_Assignment_3_WPF.Users
         public void forgotPassword()
         { }
 
-        public static string PATH_DATA = Directory.GetParent(Directory.GetParent(Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).ToString()).ToString()) + "\\Data\\";;
+        //public static string PATH_DATA = Directory.GetParent(Directory.GetParent(Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).ToString()).ToString()) + "\\Data\\";
+        public static string PATH_DATA = System.AppDomain.CurrentDomain.BaseDirectory + "Data\\";
         protected SqlConnection GetDatabaseSQLConnection()
         {
-            return new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='" + PATH_DATA + "Database.mdf';Integrated Security=True");
+            return new SqlConnection(@"Data Source=(LocalDB)\v12.0;AttachDbFilename='" + PATH_DATA + "Database.mdf';Integrated Security=True");
         }
 
         protected DataContext GetDatabaseDataContext()
         {
-            return new DataContext(@"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename='" + PATH_DATA + "Database.mdf'; Integrated Security=True");
+            return new DataContext(@"Data Source=(LocalDB)\v12.0; AttachDbFilename='" + PATH_DATA + "Database.mdf'; Integrated Security=True");
         }
 
     }
