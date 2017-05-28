c051bd4960a97457d99b28742d0d8b04acc39d83
Liew Wei Jie <liew.wj37@hotmail.com>
2017-05-28 17:35:18 +1000

    small changes

---
+++ SIT321 Assignment 3 WPF/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SIT321_Assignment_3_WPF
+{
+    static class Utilities
+    {
+        public static void GetStudentData()
+        { }
+
+        public static bool IsStudentAtRisk()
+        {
+            return false;
+        }
+
+        public static void AlertStudentAtRisk()
+        { }
+    }
+}
