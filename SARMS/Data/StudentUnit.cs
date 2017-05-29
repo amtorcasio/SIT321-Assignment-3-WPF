571cd52b0744999554436fc7883a9576c0b9984e
Andrea Michele Torcasio <torcasioandreamichele@hotmail.it>
2017-05-29 14:50:24 +1000

    Added StudentUnit data class

---
+++ SARMS/Data/StudentUnit.cs
@@ -0,0 +1,45 @@
+﻿namespace SARMS.Data
+{
+    public class StudentUnit
+    {
+        private string _userID;
+        private int _unitID;
+        private int _lectureAttendance;
+        private int _practicalAttendance;
+        private string _staffFeedback;
+        private string _studentFeedback;
+
+        #region Getters and Setters
+        public string UserID
+        {
+            get { return _userID; }
+            set { _userID = value; }
+        }
+        public int UnitID
+        {
+            get { return _unitID; }
+            set { _unitID = value; }
+        }
+        public int LectureAttendance
+        {
+            get { return _lectureAttendance; }
+            set { _lectureAttendance = value; }
+        }
+        public int PracticalAttendance
+        {
+            get { return _practicalAttendance; }
+            set { _lectureAttendance = value }
+        }
+        public string StaffFeedback
+        {
+            get { return _staffFeedback; }
+            set { _staffFeedback = value; }
+        }
+        public string StudentFeedback
+        {
+            get { return _studentFeedback; }
+            set { _studentFeedback = value; }
+        }
+        #endregion
+    }
+}
