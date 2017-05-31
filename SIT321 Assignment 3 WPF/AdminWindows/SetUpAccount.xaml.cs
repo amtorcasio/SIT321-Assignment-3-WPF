15a263e1829e1ff06f556a7d52d6b5d38baf0449
gyurikol <gyuri.kol@outlook.com>
2017-05-30 23:39:19 -0700

    setupaccount complete

--- SIT321 Assignment 3 WPF/AdminWindows/SetUpAccount.xaml.cs
+++ SIT321 Assignment 3 WPF/AdminWindows/SetUpAccount.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using SARMS;
 using SARMS.Users;
+using System.Net.Mail;
 
 namespace SIT321_Assignment_3_WPF.AdminWindows
 {
@@ -38,7 +39,34 @@ namespace SIT321_Assignment_3_WPF.AdminWindows
 
         private void btnCreateAccount_Click(object sender, RoutedEventArgs e)
         {
-            
+            // create attempt for new id
+            string id = txtFirstname.Text[0] + txtLastname.Text.Substring(0, 6);
+
+            try
+            {
+                MailAddress m = new MailAddress(txtEmail.Text);
+            }
+            catch (FormatException)
+            {
+                lblStatus.Content = "Email is incorrect format";
+                return;
+            }
+
+            // check if user id exists
+            if (Admin.SearchAccountsById(id) == null)
+            {
+                Admin.AddUser(id, txtFirstname.Text, txtLastname.Text, txtEmail.Text, psbPassword.Password, (UserType)Enum.Parse(typeof(UserType), cboAccountType.SelectedValue.ToString()));
+            }
+            else
+            {
+                int Count = 1;
+                while(Admin.SearchAccountsById(id+Count.ToString()) != null)
+                {
+                    Count++;
+                }
+
+                Admin.AddUser(id + Count, txtFirstname.Text, txtLastname.Text, txtEmail.Text, psbPassword.Password, (UserType)Enum.Parse(typeof(UserType), cboAccountType.SelectedValue.ToString()));
+            }
         }
     }
 }
