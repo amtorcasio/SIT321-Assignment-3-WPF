276c88be90e5e1c274b11dec2999cc1a99fe23a8
Liew Wei Jie <liew.wj37@hotmail.com>
2017-06-01 23:28:41 +1000

    modified color of post button in givefeedback

--- SIT321 Assignment 3 WPF/StudentWindows/GiveFeedback.xaml.cs
+++ SIT321 Assignment 3 WPF/StudentWindows/GiveFeedback.xaml.cs
@@ -17,22 +17,6 @@ using SARMS.Users;
 
 namespace SIT321_Assignment_3_WPF.StudentWindows
 {
-    public class FeedbackItem
-    {
-        public string UserName { get; set; }
-        public DateTime TimeStamp { get; set; }
-        public string PublishedComment { get; set; }
-        public List<Tuple<Account, DateTime, string>> FeedbackReplies;
-
-        public FeedbackItem(string usr, DateTime tstamp, string comment)
-        {
-            UserName = usr;
-            TimeStamp = tstamp;
-            PublishedComment = comment;
-            FeedbackReplies = new List<Tuple<Account, DateTime, string>>();
-        }
-    }
-
     /// <summary>
     /// Interaction logic for GiveFeedback.xaml
     /// </summary>
@@ -41,13 +25,6 @@ namespace SIT321_Assignment_3_WPF.StudentWindows
         public GiveFeedback()
         {
             InitializeComponent();
-
-            lsbFeedbackList.Items.Add(new FeedbackItem("John Smith", DateTime.Now, "Hello World"));
-            lsbFeedbackList.Items.Add(new FeedbackItem("Amy Marilyn", DateTime.Now, "Hello John"));
-            /*
-            lsbFeedbackList.Items.Add(new FeedbackItem("Maggie Harold", DateTime.Now, "Hello Amy"));
-            lsbFeedbackList.Items.Add(new FeedbackItem("Susan Ford", DateTime.Now, "Hello Maggie"));
-            */
         }
     }
 }
